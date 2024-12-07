e74c3a9dea9bf337cb1978eafc969ec3296657e8
jacobvanabbema <jsvanabbema@gmail.com>
2024-12-07 19:21:06 +0100

    added selection pages

---
+++ src/routes/searchMethodSelection/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Search</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>How would you like to search?</h1>
+
+	<p>
+		Please select one of the following methods.
+	</p>
+
+</div>
