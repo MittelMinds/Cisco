b0d81dbdee744d2aae15f5ece734e6237bda4f45
jacobvanabbema <jsvanabbema@gmail.com>
2024-12-07 19:34:37 +0100

    cleaned up layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	let { children } = $props();
 </script>
 
-<div class="app">
+<div>
 	<Header />
 
 	<main>
@@ -14,27 +14,17 @@
 
 	<!-- <footer>
 		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
+			Road to Start Hack, December 2024
 		</p>
 	</footer> -->
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
 
 	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
+		
 		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
 	}
 
 	footer {
@@ -43,10 +33,6 @@
 		justify-content: center;
 		align-items: center;
 		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
 	}
 
 	@media (min-width: 480px) {
--- src/routes/searchMethodSelection/+page.svelte
+++ src/routes/searchMethodSelection/+page.svelte
@@ -3,8 +3,8 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>How would you like to search?</h1>
+<div class="bg-amber-400">
+	<h1 class="text-3xl font-semibold">How would you like to search?</h1>
 
 	<p>
 		Please select one of the following methods.
