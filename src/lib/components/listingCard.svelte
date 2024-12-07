f2ab753d7c35b4b23b0baf4af3d6703ec625fbf4
jacobvanabbema <jsvanabbema@gmail.com>
2024-12-07 21:25:47 +0100

    listingcard component created

---
+++ src/lib/components/listingCard.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Card, Button, Rating, Badge } from 'flowbite-svelte';
+    export let title;
+    export let imgsrc;
+    export var starcount;
+    export let price;
+    export let prompt;
+</script>
+
+<Card padding="lg">
+	<a href="/">
+		<img class="rounded-t-lg p-8 mb-8" src={imgsrc} alt="product 1" />
+	</a>
+	<div class="px-5 pb-5">
+		<a href="/">
+			<h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+				{title}
+			</h5>
+		</a>
+		<Rating rating={starcount} size={24} class="mb-5 mt-2.5">
+			<Badge slot="text" class="ms-3">{starcount}</Badge>
+		</Rating>
+		<div class="flex items-center justify-between">
+			<span class="text-3xl font-bold text-gray-900 dark:text-white">{price}</span>
+			<Button href="/">{prompt}</Button>
+		</div>
+	</div>
+</Card>
---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import { Search, Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+	import { SearchOutline, ChevronDownOutline } from 'flowbite-svelte-icons';
+    import ListingCard from '$lib/components/listingCard.svelte';
+
+	const items = [
+		{
+			label: 'All categories'
+		},
+		{
+			label: 'Mockups'
+		},
+		{
+			label: 'Templates'
+		},
+		{
+			label: 'Design'
+		},
+		{
+			label: 'Logos'
+		}
+	];
+
+	let selectCategory = 'All categories';
+</script>
+
+<form class="flex">
+	<div class="relative">
+		<Button class="whitespace-nowrap rounded-e-none border border-e-0 border-primary-700">
+			{selectCategory}
+			<ChevronDownOutline class="ms-2.5 h-2.5 w-2.5" />
+		</Button>
+		<Dropdown classContainer="w-40">
+			{#each items as { label }}
+				<DropdownItem
+					on:click={() => {
+						selectCategory = label;
+					}}
+					class={selectCategory === label ? 'underline' : ''}
+				>
+					{label}
+				</DropdownItem>
+			{/each}
+		</Dropdown>
+	</div>
+	<Search
+		size="md"
+		class="rounded-none py-2.5"
+		placeholder="Search for Suppliers, Services, Consultants..."
+	/>
+	<Button class="rounded-s-none !p-2.5">
+		<SearchOutline class="h-6 w-6" />
+	</Button>
+</form>
+
+<ListingCard title="Brass Milling" imgsrc="https://fraesteile-marktplatz.de/wp-content/uploads/2024/09/online-fertigung-messing-fraesen.webp" starcount=4 price="ab 25 EUR" prompt="jetzt anfragen"/>
+
+
