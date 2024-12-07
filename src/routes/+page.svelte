aa5267b33e063c174c58a246aa83af70e9a265d8
thisisalexn <nuss.alex@mail.ru>
2024-12-07 20:43:32 +0100

    Dashboard added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,47 +3,16 @@
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcomeFallback from '$lib/images/svelte-welcome.png';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import Dashboard from './Dashboard.svelte';
 </script>
 
 <svelte:head>
-	<title>Home home</title>
+	<title>Dashbaord</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<Navbar>
-		<NavBrand href="/">
-		  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzJTZ4coqMYM5TqZceNVOu-M-issSAjZJ84Q&s" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-		  <span class="self-center whitespace-nowrap text-black text-xl font-semibold dark:text-white">Flowbite</span>
-		</NavBrand>
-		<NavHamburger  />
-		<NavUl >
-		  <NavLi href="/">Home</NavLi>
-		  <NavLi href="/about">About</NavLi>
-		  <NavLi href="/docs/components/navbar">Navbar</NavLi>
-		  <NavLi href="/pricing">Pricing</NavLi>
-		  <NavLi href="/contact">Contact</NavLi>
-		</NavUl>
-	  </Navbar>
-	<h1 class="text-3xl font-bold underline">
-		hello there
-	</h1>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+ <Dashboard/>
 </section>
 
 <style>
