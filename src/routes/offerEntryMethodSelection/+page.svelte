60d40e1ad8ecad1c239f22a1b2d90343b050f4fd
jacobvanabbema <jsvanabbema@gmail.com>
2024-12-07 20:52:51 +0100

    finished selection pages mvp

--- src/routes/offerEntryMethodSelection/+page.svelte
+++ src/routes/offerEntryMethodSelection/+page.svelte
@@ -1,26 +1,30 @@
+<script>
+	import { Card, Button, Toggle } from 'flowbite-svelte';
+	import { ArrowRightOutline, FilePenOutline } from 'flowbite-svelte-icons';
+
+	import SelectionCard from "/src/lib/components/selectionCard.svelte"
+	
+  </script>
+
 <svelte:head>
-	<title>Offer Entry</title>
+	<title>Search</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>Enter your offer</h1>
+<div class="ml-8 mt-4">
+	<h1 class="text-3xl font-semibold mb-2">How would you like to upload your listing?</h1>
 
-	<p>
-		This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. You can make your own by typing
-		the following into your command line and following the prompts:
+	<p class="mb-6">
+		Please select one of the following methods. 
 	</p>
 
-	<pre>npx sv create</pre>
+	<div class="flex space-x-4">
+					
+			<SelectionCard imgsrc="/src/lib/images/ai_bot_image.webp" title="Use our AI Assistant" subtitle="Create the perfect listing with our AI." buttonlabel="Let's go"/>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+			<SelectionCard imgsrc="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png" title="Enter manually" subtitle="Fill out a simple form." buttonlabel="Search"/>
+		
+	</div>
 
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+
 </div>
--- src/routes/searchMethodSelection/+page.svelte
+++ src/routes/searchMethodSelection/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { Card, Button, Toggle } from 'flowbite-svelte';
 	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
+	import SelectionCard from "/src/lib/components/selectionCard.svelte"
 	
   </script>
 
@@ -9,38 +11,19 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="bg-amber-400">
+<div class="ml-8 mt-4">
 	<h1 class="text-3xl font-semibold mb-2">How would you like to search?</h1>
 
-	<p class="mb-4">
+	<p class="mb-6">
 		Please select one of the following methods.
 	</p>
 
 	<div class="flex space-x-4">
-		<div>
+					
+			<SelectionCard imgsrc="/src/lib/images/ai_bot_image.webp" title="Use our AI Assistant" subtitle="Let it interactively guide you towards finding the perfect partner." buttonlabel="Let's go"/>
 
-			<Card class="">
-			<img class="h-28 w-28" src="/src/lib/images/ai_bot_image.webp" alt="bot">
-			  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Use our AI Assistant</h5>
-			  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Let it interactively guide you towards finding the perfect partner.</p>
-			  <Button>
-				Let's go <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
-			  </Button>
-			</Card>
-			
-		  </div>
-
-		  <div class="">
-			<Card>
-			<img class="h-28 w-28" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png" alt="bot">
-			  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Search manually</h5>
-			  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Use keywords and filters to find what you're looking for.</p>
-			  <Button>
-				Start search
-			  </Button>
-			</Card>
-			
-		  </div>
+			<SelectionCard imgsrc="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png" title="Search manually" subtitle="Use keywords and filters to find what you're looking for." buttonlabel="Search"/>
+		
 	</div>
 
 
